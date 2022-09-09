2e5b063f019ae111cfc3fff81a3a743f0dfb5c5c
pilot <peterajani164@gmail.com>
2022-09-09 16:08:35 +0100

    i finished the interface and slight javascript

---
+++ stopwatch.js
@@ -0,0 +1,23 @@
+
+
+
+const stopWatch = () =>{
+    let countedSec = mySecs.value;
+    countedSec++;
+    mySecs.value = countedSec;
+    setTimeout(stopWatch, 10)
+    if(mySecs.value == 100){
+        mySecs.value = 0;
+        myMin.value++;
+    }
+    if(myMin.value == 60){
+        myMin.value = 0;
+        myHour.value++
+    }
+}
+
+const stop =()=>{
+    myHour.innerText = myHour
+    myMin.innerText = myMin
+    mySecs.innerText = my
+}
